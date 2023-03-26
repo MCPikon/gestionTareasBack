5fb8bc80d225d1014d168dc9eeaaee78da58d876
Javier Picón <82212139+MCPikon@users.noreply.github.com>
2023-03-26 15:01:39 +0200

    getByEmail added in controller and bug fixes

--- src/main/java/com/jpicon/gestionTareas/controllers/TareasController.java
+++ src/main/java/com/jpicon/gestionTareas/controllers/TareasController.java
@@ -4,14 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.jpicon.gestionTareas.entities.Tarea;
 import com.jpicon.gestionTareas.entities.Usuario;
@@ -21,6 +14,7 @@ import com.jpicon.gestionTareas.services.TareasService;
 
 @RestController
 @RequestMapping("/tareas")
+@CrossOrigin(origins = "*")
 public class TareasController {
 	
 	@Autowired
--- src/main/java/com/jpicon/gestionTareas/controllers/UsuariosController.java
+++ src/main/java/com/jpicon/gestionTareas/controllers/UsuariosController.java
@@ -4,14 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.jpicon.gestionTareas.entities.Usuario;
 import com.jpicon.gestionTareas.model.ErrorException;
@@ -20,6 +13,7 @@ import com.jpicon.gestionTareas.services.UsuariosService;
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin(origins = "*")
 public class UsuariosController {
 
 	@Autowired
@@ -39,6 +33,15 @@ public class UsuariosController {
 	public ResponseEntity<?> findById(@PathVariable("id") int id) {
 		try {
 			return ResponseEntity.ok(usersService.findById(id));
+		} catch (ErrorException e) {
+			return new ResponseEntity<ResponseBase>(new ResponseBase(e), e.getIdStatus());
+		}
+	}
+
+	@GetMapping("/getByEmail/{email}")
+	public ResponseEntity<?> getByEmail(@PathVariable("email") String email) {
+		try {
+			return ResponseEntity.ok(usersService.getByEmail(email));
 		} catch (ErrorException e) {
 			return new ResponseEntity<ResponseBase>(new ResponseBase(e), e.getIdStatus());
 		}
